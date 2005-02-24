2c04b0d2621480db11dad3ec00b203ce531746db
hwyss <hwyss@gentoo.hwyss.net>
2005-02-24 16:33:48 +0100

    ChangeSet 1.6: never allow empty user-name or email lib/rrba/user.rb: never allow empty user-name or email

--- lib/rrba/user.rb
+++ lib/rrba/user.rb
@@ -3,8 +3,7 @@
 
 module RRBA
 	class User
-		attr_accessor :name, :email, :short_name
-		attr_reader :unique_id, :public_key
+		attr_reader :name, :email, :short_name, :unique_id, :public_key
 		def initialize(unique_id)
 			@unique_id = unique_id or raise "Invalid ID #{unique_id}"
 		end
@@ -13,12 +12,27 @@ module RRBA
 			# (for subclasses that can be persisted)
 			public_key.sysverify(challenge, signature)
 		end
+		def email=(email)
+			email = email.to_s.strip
+			raise "Invalid Email #{email}" if(email.empty?)
+			@email = email
+		end
+		def name=(name)
+			name = name.to_s.strip
+			raise "Invalid Username #{name}" if(name.empty?)
+			@name = name
+		end
 		def new_session
 			raise NotImplementedError, 
 				"no predefined behavior for RRBA::User#new_session"
 		end
 		def public_key=(key)
 			@public_key = key.public_key
+		end
+		def short_name=(short_name)
+			short_name = short_name.to_s.strip
+			raise "Invalid Short Name #{short_name}" if(short_name.empty?)
+			@short_name = short_name
 		end
 	end
 end
